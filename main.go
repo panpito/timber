dc1aa6af7732a12fdd4692e20da6c6f2bfd8b41c
Hervinator <hervito.dela.vega@gmail.com>
2023-01-04 16:20:18 +0000

    Initial seed

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/panpito/timber/timber"
+)
+
+func main() {
+	timber.Hello()
+}
---
+++ timber/timber.go
@@ -0,0 +1,7 @@
+package timber
+
+import log "github.com/sirupsen/logrus"
+
+func Hello() {
+	log.Print("hello from timber package")
+}
